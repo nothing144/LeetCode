ee2205fc8bf6bb916a5f88d0aba9f85900d4ad4b
Harsh pandey <thepandey144@gmail.com>
2026-08-07 08:23:57 +0530

    Automated Sync: Permutation in String (Java)

--- 567-permutation-in-string/solution.java
+++ 567-permutation-in-string/solution.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0; i<s1.length(); i++){
             map1[s1.charAt(i)-'a']++;
         }
-       for(int i=0; i<s2.length()-s1.length(); i++){
+       for(int i=0; i<=s2.length()-s1.length(); i++){
           int[] map2 = new int[26];
           for(int j=0; j<s1.length(); j++){
             map2[s2.charAt(i+j)-'a']++;
